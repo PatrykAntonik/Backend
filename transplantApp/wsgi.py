76490dbc82ceee45f500377d0da3e81bf5aa1da4
PatrykAntonik <patryka2000@gmail.com>
2024-03-06 21:22:22 +0100

    replaced deployment py with plain settings

--- transplantApp/wsgi.py
+++ transplantApp/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'transplantApp.settings'
+settings_module = 'transplantApp.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'transplantApp.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
