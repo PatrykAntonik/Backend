4aff766c6470ec4abe4115dd78352f5a854073d2
Patryk <patryka2000@gmail.com>
2025-10-03 11:26:32 +0200

    fixed db config

--- transplantApp/settings.py
+++ transplantApp/settings.py
@@ -134,12 +134,12 @@ WSGI_APPLICATION = 'transplantApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -150,16 +150,7 @@ WSGI_APPLICATION = 'transplantApp.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'transplantApp',
-        'USER': 'patryka2000',
-        'PASSWORD': '9DF5ftexhYJa',
-        'HOST': 'ep-aged-wind-a2jpgyck.eu-central-1.aws.neon.tech',
-        'PORT': '5432',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
